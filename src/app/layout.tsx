306f2331f964e22d3cd9999623bbf495c38e26c0
deivison resende <deivison.resende@enacom.com.br>
2023-05-19 21:01:02 -0300

    feat: setup app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,14 @@ import {
   Roboto_Flex as Roboto,
 } from 'next/font/google'
 
+import { AbsolutePurpleBlur } from '@/components/AbsolutePurpleBlur'
+import { AbsoluteVerticalStripes } from '@/components/AbsoluteVerticalStripes'
+import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 import { ReactNode } from 'react'
+import { SignIn } from '@/components/SignIn'
+import { getUser } from '@/lib/auth'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -21,12 +28,28 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const user = getUser()
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {user ? <Profile data={user} /> : <SignIn />}
+            <Hero />
+            <Copyright />
+
+            <AbsolutePurpleBlur />
+            <AbsoluteVerticalStripes />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,5 @@
-import { AbsolutePurpleBlur } from '@/components/AbsolutePurpleBlur'
-import { AbsoluteVerticalStripes } from '@/components/AbsoluteVerticalStripes'
-import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
-import { Hero } from '@/components/Hero'
-import { Profile } from '@/components/Profile'
-import { SignIn } from '@/components/SignIn'
-import { getUser } from '@/lib/auth'
 
 export default function Home() {
-  const user = getUser()
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        {user ? <Profile data={user} /> : <SignIn />}
-        <Hero />
-        <Copyright />
-
-        <AbsolutePurpleBlur />
-        <AbsoluteVerticalStripes />
-      </div>
-
-      {/* Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
